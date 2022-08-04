02770e3cad47b96e581c6c29ba3fadc8c7336f8e
Tom Blakely <54464150+Tb243@users.noreply.github.com>
2022-08-04 17:57:40 +1000

    Update material version to 1.6.1 to allow for Material You theming Create test activity

--- app/src/main/java/com/example/teamguardians/LoginActivity.java
+++ app/src/main/java/com/example/teamguardians/LoginActivity.java
@@ -5,11 +5,11 @@ import android.app.Application;
 import com.google.android.material.color.DynamicColors;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
