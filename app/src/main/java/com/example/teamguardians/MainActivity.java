e2040dc2425bc98e25f42c12428f6c0ad20a0850
Tom Blakely <54464150+Tb243@users.noreply.github.com>
2022-08-15 19:05:00 +1000

    Create initial splash

--- app/src/main/java/com/example/teamguardians/MainActivity.java
+++ app/src/main/java/com/example/teamguardians/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.teamguardians;
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.Application;
 import com.google.android.material.color.DynamicColors;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button getStartedButton = findViewById(R.id.getStartedButton);
+
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent getStartedIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(getStartedIntent);
+            }
+        });
     }
 }
