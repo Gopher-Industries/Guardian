bfcb4282f57d7e50e4b94ccf4b42aabc62733dd9
Tom Blakely <54464150+Tb243@users.noreply.github.com>
2022-08-16 20:17:47 +1000

    Creating initial register account activity

--- app/src/main/java/com/example/teamguardians/LoginActivity.java
+++ app/src/main/java/com/example/teamguardians/LoginActivity.java
@@ -1,9 +1,16 @@
 package com.example.teamguardians;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.app.Application;
 import com.google.android.material.color.DynamicColors;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -11,5 +18,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button LoginButton = findViewById(R.id.LoginButton);
+        Button RegisterButton = findViewById(R.id.RegisterButton);
+
+        RegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
