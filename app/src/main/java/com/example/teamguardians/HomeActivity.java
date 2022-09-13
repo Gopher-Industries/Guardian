f947a93230e78de273dadd4a8b236d486160e5f9
Tom Blakely <54464150+Tb243@users.noreply.github.com>
2022-09-13 22:37:32 +1000

    CareTakerProfile Activity linked for testing

--- app/src/main/java/com/example/teamguardians/HomeActivity.java
+++ app/src/main/java/com/example/teamguardians/HomeActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
@@ -15,11 +16,20 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         ImageButton NewPatientButton = findViewById(R.id.NewPatientButton);
+        Button ProfileButton = findViewById(R.id.ProfileButton);
         NewPatientButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent NewPatientIntent = new Intent(HomeActivity.this, NewPatientActivity.class);
                 startActivity(NewPatientIntent);
+            }
+        });
+
+        ProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent ProfileIntent = new Intent(HomeActivity.this, CareTakerProfile.class);
+                startActivity(ProfileIntent);
             }
         });
     }
