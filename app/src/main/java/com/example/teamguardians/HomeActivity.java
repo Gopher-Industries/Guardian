700ce88c54fac383e27bfd142478839c5051c583
Tom Blakely <54464150+Tb243@users.noreply.github.com>
2022-09-13 21:32:37 +1000

    Testing with themes / create new patient activity

--- app/src/main/java/com/example/teamguardians/HomeActivity.java
+++ app/src/main/java/com/example/teamguardians/HomeActivity.java
@@ -2,7 +2,10 @@ package com.example.teamguardians;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageButton NewPatientButton = findViewById(R.id.NewPatientButton);
+        NewPatientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent NewPatientIntent = new Intent(HomeActivity.this, NewPatientActivity.class);
+                startActivity(NewPatientIntent);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/teamguardians/NewPatientActivity.java
@@ -0,0 +1,14 @@
+package com.example.teamguardians;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewPatientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_patient);
+    }
+}
