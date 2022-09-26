c1a41ac623725f498779f80567b78a7e6026a5f4
Tom Blakely <54464150+Tb243@users.noreply.github.com>
2022-09-26 16:26:07 +1000

    refactoring and implement Next button

--- app/src/main/java/com/example/tg_patient_profile/Careplan1.java
+++ app/src/main/java/com/example/tg_patient_profile/Careplan1.java
@@ -1,6 +1,9 @@
 package com.example.tg_patient_profile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class Careplan1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_careplan1);
+
+        Button careplan1NextButton = findViewById(R.id.careplan1NextButton);
+
+        careplan1NextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent careplan2Intent = new Intent(Careplan1.this, Careplan2.class);
+                startActivity(careplan2Intent);
+            }
+        });
     }
 }
