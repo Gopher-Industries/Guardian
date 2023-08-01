b4293b2fafb9fafba66add2d6ca3dfee1de3fadf
Darshan <109759629+DARSHcverse@users.noreply.github.com>
2023-08-01 20:12:48 +1000

    Profile Image XML Design
    Profile Image
    XML Design is made, next is to firebase.

--- app/src/main/java/com/example/tg_patient_profile/view/general/UploadPhoto.java
+++ app/src/main/java/com/example/tg_patient_profile/view/general/UploadPhoto.java
@@ -3,9 +3,11 @@ package com.example.tg_patient_profile.view.general;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +23,7 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
@@ -31,6 +34,7 @@ public class UploadPhoto extends AppCompatActivity {
     StorageReference storageReference;
     ProgressDialog progressDialog;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,71 +42,19 @@ public class UploadPhoto extends AppCompatActivity {
         binding = ActivityUploadPhotoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.selectImagebtn.setOnClickListener(new View.OnClickListener() {
+        binding.profilepic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selectImage();
             }
         });
-
-        binding.uploadimagebtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                uploadImage();
-            }
-        });
     }
 
-    private void uploadImage() {
-
-        progressDialog = new ProgressDialog(this);
-        progressDialog.setTitle("Uploading File....");
-        progressDialog.show();
-
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.CANADA);
-        Date now = new Date();
-        String fileName = formatter.format(now);
-        storageReference = FirebaseStorage.getInstance().getReference("gp_photos/"+fileName); //my personal Firebase: proj: underCare --> Storage --- gp_photos
-
-        storageReference.putFile(imageUri)
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-
-                        binding.firebaseimage.setImageURI(null);
-                        Toast.makeText(UploadPhoto.this,"Successfully Uploaded",Toast.LENGTH_SHORT).show();
-                        if (progressDialog.isShowing())
-                            progressDialog.dismiss();
-
-                        final TextView textView = (TextView) findViewById(R.id.textView); //get the id for TextView
-                        textView.setText(imageUri.toString());
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        if (progressDialog.isShowing())
-                            progressDialog.dismiss();
-                        Toast.makeText(UploadPhoto.this,"Failed to Upload",Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
-
-    private void selectImage() {
-
+    private void selectImage(){
         Intent intent = new Intent();
         intent.setType("gp_photos/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent,100);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == 100 && data != null && data.getData() != null){
-
-            imageUri = data.getData();
-            binding.firebaseimage.setImageURI(imageUri);
-        }
-    }
 }
