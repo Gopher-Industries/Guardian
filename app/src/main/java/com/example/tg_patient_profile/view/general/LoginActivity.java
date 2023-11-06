1be77e1afeaa61984a6891d255ef069d7dbf43cd
Lachlan Vass <lachlan.vass@okendo.io>
2023-11-06 23:18:46 +1000

    formatting

--- app/src/main/java/com/example/tg_patient_profile/view/general/LoginActivity.java
+++ app/src/main/java/com/example/tg_patient_profile/view/general/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.tg_patient_profile.view.general;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -14,16 +13,11 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.tg_patient_profile.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -49,156 +43,122 @@ public class LoginActivity extends AppCompatActivity {
     role_radioGroup = findViewById(R.id.login_role_radioGroup);
 
     mLoginBtn.setOnClickListener(
-        new View.OnClickListener() {
-          @Override
-          public void onClick(final View v) {
+        v -> {
+          final String email = mEmail.getText().toString().trim();
+          final String password = mPassword.getText().toString().trim();
+          final String role;
+          final int selectedRadioButtonId = role_radioGroup.getCheckedRadioButtonId();
+          if (-1 != selectedRadioButtonId) {
+            final RadioButton seletedRadioButton = findViewById(selectedRadioButtonId);
+            role = seletedRadioButton.getText().toString();
+            final SharedPreferences sharedPreferences =
+                getSharedPreferences("MY_PREF", Context.MODE_PRIVATE);
+            final SharedPreferences.Editor editor = sharedPreferences.edit();
 
-            final String email = mEmail.getText().toString().trim();
-            final String password = mPassword.getText().toString().trim();
-            String role = "";
-            final int selectedRadioButtonId = role_radioGroup.getCheckedRadioButtonId();
-            if (-1 != selectedRadioButtonId) {
-              final RadioButton seletedRadioButton = findViewById(selectedRadioButtonId);
-              role = seletedRadioButton.getText().toString();
-              final SharedPreferences sharedPreferences =
-                  getSharedPreferences("MY_PREF", Context.MODE_PRIVATE);
-              final SharedPreferences.Editor editor = sharedPreferences.edit();
+            if (role.equals("Caretaker")) {
+              editor.putInt("login_role", 0);
 
-              if (role.equals("Caretaker")) {
-                editor.putInt("login_role", 0);
-
-              } else {
-                editor.putInt("login_role", 1);
-              }
-              editor.apply();
             } else {
-              Toast.makeText(LoginActivity.this, "please choose a role", Toast.LENGTH_SHORT).show();
-              return;
+              editor.putInt("login_role", 1);
             }
-
-            if (TextUtils.isEmpty(email)) {
-              mEmail.setError("Email is Required.");
-              return;
-            }
-
-            if (TextUtils.isEmpty(password)) {
-              mPassword.setError("Password is Required.");
-              return;
-            }
-
-            if (6 > password.length()) {
-              mPassword.setError("Password Must be >= 6 Characters");
-              return;
-            }
-
-            progressBar.setVisibility(View.VISIBLE);
-
-            // authenticate the user
-
-            Auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(
-                    new OnCompleteListener<AuthResult>() {
-                      @Override
-                      public void onComplete(@NonNull final Task<AuthResult> task) {
-                        String role = "";
-                        final int selectedRadioButtonId = role_radioGroup.getCheckedRadioButtonId();
-                        if (-1 != selectedRadioButtonId) {
-                          final RadioButton seletedRadioButton =
-                              findViewById(selectedRadioButtonId);
-                          role = seletedRadioButton.getText().toString();
-                        }
-                        if (task.isSuccessful()) {
-                          Toast.makeText(
-                                  LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT)
-                              .show();
-                          if (role.equals("Caretaker")) {
-                            Toast.makeText(
-                                    LoginActivity.this,
-                                    "Logged in as CareTaker",
-                                    Toast.LENGTH_SHORT)
-                                .show();
-                            startActivity(
-                                new Intent(getApplicationContext(), Homepage4caretaker.class));
-                          } else {
-                            startActivity(
-                                new Intent(getApplicationContext(), Homepage4admin.class));
-                          }
-                        } else {
-                          Toast.makeText(
-                                  LoginActivity.this,
-                                  "Error ! " + task.getException().getMessage(),
-                                  Toast.LENGTH_SHORT)
-                              .show();
-                          progressBar.setVisibility(View.GONE);
-                        }
-                      }
-                    });
+            editor.apply();
+          } else {
+            Toast.makeText(LoginActivity.this, "please choose a role", Toast.LENGTH_SHORT).show();
+            return;
           }
+
+          if (TextUtils.isEmpty(email)) {
+            mEmail.setError("Email is Required.");
+            return;
+          }
+
+          if (TextUtils.isEmpty(password)) {
+            mPassword.setError("Password is Required.");
+            return;
+          }
+
+          if (6 > password.length()) {
+            mPassword.setError("Password Must be >= 6 Characters");
+            return;
+          }
+
+          progressBar.setVisibility(View.VISIBLE);
+
+          // authenticate the user
+
+          Auth.signInWithEmailAndPassword(email, password)
+              .addOnCompleteListener(
+                  task -> {
+                    String role1 = "";
+                    final int selectedRadioButtonId1 = role_radioGroup.getCheckedRadioButtonId();
+                    if (-1 != selectedRadioButtonId1) {
+                      final RadioButton seletedRadioButton = findViewById(selectedRadioButtonId1);
+                      role1 = seletedRadioButton.getText().toString();
+                    }
+                    if (task.isSuccessful()) {
+                      Toast.makeText(
+                              LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT)
+                          .show();
+                      if (role1.equals("Caretaker")) {
+                        Toast.makeText(
+                                LoginActivity.this, "Logged in as CareTaker", Toast.LENGTH_SHORT)
+                            .show();
+                        startActivity(
+                            new Intent(getApplicationContext(), Homepage4caretaker.class));
+                      } else {
+                        startActivity(new Intent(getApplicationContext(), Homepage4admin.class));
+                      }
+                    } else {
+                      Toast.makeText(
+                              LoginActivity.this,
+                              "Error ! " + Objects.requireNonNull(task.getException()).getMessage(),
+                              Toast.LENGTH_SHORT)
+                          .show();
+                      progressBar.setVisibility(View.GONE);
+                    }
+                  });
         });
 
     mCreateBtn.setOnClickListener(
-        new View.OnClickListener() {
-          @Override
-          public void onClick(final View v) {
-            startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
-          }
-        });
+        v -> startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
 
     forgotTextLink.setOnClickListener(
-        new View.OnClickListener() {
-          @Override
-          public void onClick(final View v) {
+        v -> {
+          final EditText resetMail = new EditText(v.getContext());
+          final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+          passwordResetDialog.setTitle("Reset Password ?");
+          passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+          passwordResetDialog.setView(resetMail);
 
-            final EditText resetMail = new EditText(v.getContext());
-            final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
-            passwordResetDialog.setTitle("Reset Password ?");
-            passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
-            passwordResetDialog.setView(resetMail);
+          passwordResetDialog.setPositiveButton(
+              "Yes",
+              (dialog, which) -> {
+                // extract the email and send reset link
+                final String mail = resetMail.getText().toString();
+                Auth.sendPasswordResetEmail(mail)
+                    .addOnSuccessListener(
+                        aVoid ->
+                            Toast.makeText(
+                                    LoginActivity.this,
+                                    "Reset Link Sent To Your Email.",
+                                    Toast.LENGTH_SHORT)
+                                .show())
+                    .addOnFailureListener(
+                        e ->
+                            Toast.makeText(
+                                    LoginActivity.this,
+                                    "Error ! Reset Link is Not Sent" + e.getMessage(),
+                                    Toast.LENGTH_SHORT)
+                                .show());
+              });
 
-            passwordResetDialog.setPositiveButton(
-                "Yes",
-                new DialogInterface.OnClickListener() {
-                  @Override
-                  public void onClick(final DialogInterface dialog, final int which) {
-                    // extract the email and send reset link
-                    final String mail = resetMail.getText().toString();
-                    Auth.sendPasswordResetEmail(mail)
-                        .addOnSuccessListener(
-                            new OnSuccessListener<Void>() {
-                              @Override
-                              public void onSuccess(final Void aVoid) {
-                                Toast.makeText(
-                                        LoginActivity.this,
-                                        "Reset Link Sent To Your Email.",
-                                        Toast.LENGTH_SHORT)
-                                    .show();
-                              }
-                            })
-                        .addOnFailureListener(
-                            new OnFailureListener() {
-                              @Override
-                              public void onFailure(@NonNull final Exception e) {
-                                Toast.makeText(
-                                        LoginActivity.this,
-                                        "Error ! Reset Link is Not Sent" + e.getMessage(),
-                                        Toast.LENGTH_SHORT)
-                                    .show();
-                              }
-                            });
-                  }
-                });
+          passwordResetDialog.setNegativeButton(
+              "No",
+              (dialog, which) -> {
+                // close the dialog
+              });
 
-            passwordResetDialog.setNegativeButton(
-                "No",
-                new DialogInterface.OnClickListener() {
-                  @Override
-                  public void onClick(final DialogInterface dialog, final int which) {
-                    // close the dialog
-                  }
-                });
-
-            passwordResetDialog.create().show();
-          }
+          passwordResetDialog.create().show();
         });
   }
 }
