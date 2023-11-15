bced30c09b695be53f99265a9e0c2663f7d10240
Lachlan Vass <lachlan.vass@okendo.io>
2023-11-15 20:41:00 +1000

    android studio convert to Kotlin

--- app/src/main/java/deakin/gopher/guardian/view/general/LoginActivity.kt
+++ app/src/main/java/deakin/gopher/guardian/view/general/LoginActivity.kt
@@ -1,164 +1,158 @@
-package deakin.gopher.guardian.view.general;
+package deakin.gopher.guardian.view.general
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.auth.FirebaseAuth;
-import deakin.gopher.guardian.R;
-import java.util.Objects;
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import deakin.gopher.guardian.R
+import java.util.Objects
 
-public class LoginActivity extends AppCompatActivity {
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val mEmail: EditText = findViewById(R.id.Email)
+        val mPassword: EditText = findViewById(R.id.password)
+        val progressBar: ProgressBar = findViewById(R.id.progressBar)
+        val Auth = FirebaseAuth.getInstance()
+        val mLoginBtn: Button = findViewById(R.id.loginBtn)
+        val mCreateBtn: Button = findViewById(R.id.createText)
+        val forgotTextLink: TextView = findViewById(R.id.forgotPassword)
+        val role_radioGroup: RadioGroup = findViewById(R.id.login_role_radioGroup)
 
-  EditText mEmail, mPassword;
-  Button mLoginBtn;
-  TextView mCreateBtn, forgotTextLink;
-  ProgressBar progressBar;
-  FirebaseAuth Auth;
-  RadioGroup role_radioGroup;
 
-  @Override
-  protected void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_login);
-
-    mEmail = findViewById(R.id.Email);
-    mPassword = findViewById(R.id.password);
-    progressBar = findViewById(R.id.progressBar);
-    Auth = FirebaseAuth.getInstance();
-    mLoginBtn = findViewById(R.id.loginBtn);
-    mCreateBtn = findViewById(R.id.createText);
-    forgotTextLink = findViewById(R.id.forgotPassword);
-    role_radioGroup = findViewById(R.id.login_role_radioGroup);
-
-    mLoginBtn.setOnClickListener(
-        v -> {
-          final String email = mEmail.getText().toString().trim();
-          final String password = mPassword.getText().toString().trim();
-          final String role;
-          final int selectedRadioButtonId = role_radioGroup.getCheckedRadioButtonId();
-          if (-1 != selectedRadioButtonId) {
-            final RadioButton seletedRadioButton = findViewById(selectedRadioButtonId);
-            role = seletedRadioButton.getText().toString();
-            final SharedPreferences sharedPreferences =
-                getSharedPreferences("MY_PREF", Context.MODE_PRIVATE);
-            final SharedPreferences.Editor editor = sharedPreferences.edit();
-
-            if (role.equals("Caretaker")) {
-              editor.putInt("login_role", 0);
-
+        mLoginBtn.setOnClickListener {
+            val email = mEmail.text.toString().trim { it <= ' ' }
+            val password = mPassword.text.toString().trim { it <= ' ' }
+            val role: String
+            val selectedRadioButtonId = role_radioGroup.checkedRadioButtonId
+            if (-1 != selectedRadioButtonId) {
+                val seletedRadioButton = findViewById<RadioButton>(selectedRadioButtonId)
+                role = seletedRadioButton.text.toString()
+                val sharedPreferences = getSharedPreferences("MY_PREF", MODE_PRIVATE)
+                val editor = sharedPreferences.edit()
+                if (role == "Caretaker") {
+                    editor.putInt("login_role", 0)
+                } else {
+                    editor.putInt("login_role", 1)
+                }
+                editor.apply()
             } else {
-              editor.putInt("login_role", 1);
+                Toast.makeText(this@LoginActivity, "please choose a role", Toast.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
             }
-            editor.apply();
-          } else {
-            Toast.makeText(LoginActivity.this, "please choose a role", Toast.LENGTH_SHORT).show();
-            return;
-          }
+            if (TextUtils.isEmpty(email)) {
+                mEmail.error = "Email is Required."
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)) {
+                mPassword.error = "Password is Required."
+                return@setOnClickListener
+            }
+            if (6 > password.length) {
+                mPassword.error = "Password Must be >= 6 Characters"
+                return@setOnClickListener
+            }
+            progressBar.visibility = View.VISIBLE
 
-          if (TextUtils.isEmpty(email)) {
-            mEmail.setError("Email is Required.");
-            return;
-          }
-
-          if (TextUtils.isEmpty(password)) {
-            mPassword.setError("Password is Required.");
-            return;
-          }
-
-          if (6 > password.length()) {
-            mPassword.setError("Password Must be >= 6 Characters");
-            return;
-          }
-
-          progressBar.setVisibility(View.VISIBLE);
-
-          // authenticate the user
-
-          Auth.signInWithEmailAndPassword(email, password)
-              .addOnCompleteListener(
-                  task -> {
-                    String role1 = "";
-                    final int selectedRadioButtonId1 = role_radioGroup.getCheckedRadioButtonId();
+            // authenticate the user
+            Auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task: Task<AuthResult?> ->
+                    var role1 = ""
+                    val selectedRadioButtonId1 = role_radioGroup.checkedRadioButtonId
                     if (-1 != selectedRadioButtonId1) {
-                      final RadioButton seletedRadioButton = findViewById(selectedRadioButtonId1);
-                      role1 = seletedRadioButton.getText().toString();
+                        val seletedRadioButton =
+                            findViewById<RadioButton>(selectedRadioButtonId1)
+                        role1 = seletedRadioButton.text.toString()
                     }
-                    if (task.isSuccessful()) {
-                      Toast.makeText(
-                              LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT)
-                          .show();
-                      if (role1.equals("Caretaker")) {
+                    if (task.isSuccessful) {
                         Toast.makeText(
-                                LoginActivity.this, "Logged in as CareTaker", Toast.LENGTH_SHORT)
-                            .show();
-                        startActivity(
-                            new Intent(getApplicationContext(), Homepage4caretaker.class));
-                      } else {
-                        startActivity(new Intent(getApplicationContext(), Homepage4admin.class));
-                      }
+                            this@LoginActivity, "Logged in Successfully", Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        if (role1 == "Caretaker") {
+                            Toast.makeText(
+                                this@LoginActivity, "Logged in as CareTaker", Toast.LENGTH_SHORT
+                            )
+                                .show()
+                            startActivity(
+                                Intent(applicationContext, Homepage4caretaker::class.java)
+                            )
+                        } else {
+                            startActivity(
+                                Intent(
+                                    applicationContext,
+                                    Homepage4admin::class.java
+                                )
+                            )
+                        }
                     } else {
-                      Toast.makeText(
-                              LoginActivity.this,
-                              "Error ! " + Objects.requireNonNull(task.getException()).getMessage(),
-                              Toast.LENGTH_SHORT)
-                          .show();
-                      progressBar.setVisibility(View.GONE);
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Error ! " + Objects.requireNonNull(task.exception).message,
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        progressBar.visibility = View.GONE
                     }
-                  });
-        });
-
-    mCreateBtn.setOnClickListener(
-        v -> startActivity(new Intent(getApplicationContext(), RegisterActivity.class)));
-
-    forgotTextLink.setOnClickListener(
-        v -> {
-          final EditText resetMail = new EditText(v.getContext());
-          final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
-          passwordResetDialog.setTitle("Reset Password ?");
-          passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
-          passwordResetDialog.setView(resetMail);
-
-          passwordResetDialog.setPositiveButton(
-              "Yes",
-              (dialog, which) -> {
-                // extract the email and send reset link
-                final String mail = resetMail.getText().toString();
-                Auth.sendPasswordResetEmail(mail)
-                    .addOnSuccessListener(
-                        aVoid ->
+                }
+        }
+        mCreateBtn.setOnClickListener(
+            View.OnClickListener { v: View? ->
+                startActivity(
+                    Intent(
+                        applicationContext, RegisterActivity::class.java
+                    )
+                )
+            })
+        forgotTextLink.setOnClickListener(
+            View.OnClickListener { v: View ->
+                val resetMail = EditText(v.context)
+                val passwordResetDialog = AlertDialog.Builder(v.context)
+                passwordResetDialog.setTitle("Reset Password ?")
+                passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.")
+                passwordResetDialog.setView(resetMail)
+                passwordResetDialog.setPositiveButton(
+                    "Yes"
+                ) { dialog: DialogInterface?, which: Int ->
+                    // extract the email and send reset link
+                    val mail = resetMail.text.toString()
+                    Auth.sendPasswordResetEmail(mail)
+                        .addOnSuccessListener { aVoid: Void? ->
                             Toast.makeText(
-                                    LoginActivity.this,
-                                    "Reset Link Sent To Your Email.",
-                                    Toast.LENGTH_SHORT)
-                                .show())
-                    .addOnFailureListener(
-                        e ->
+                                this@LoginActivity,
+                                "Reset Link Sent To Your Email.",
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }
+                        .addOnFailureListener { e: Exception ->
                             Toast.makeText(
-                                    LoginActivity.this,
-                                    "Error ! Reset Link is Not Sent" + e.getMessage(),
-                                    Toast.LENGTH_SHORT)
-                                .show());
-              });
-
-          passwordResetDialog.setNegativeButton(
-              "No",
-              (dialog, which) -> {
-                // close the dialog
-              });
-
-          passwordResetDialog.create().show();
-        });
-  }
+                                this@LoginActivity,
+                                "Error ! Reset Link is Not Sent" + e.message,
+                                Toast.LENGTH_SHORT
+                            )
+                                .show()
+                        }
+                }
+                passwordResetDialog.setNegativeButton(
+                    "No"
+                ) { dialog: DialogInterface?, which: Int -> }
+                passwordResetDialog.create().show()
+            })
+    }
 }
