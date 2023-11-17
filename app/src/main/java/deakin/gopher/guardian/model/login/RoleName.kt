cb6146d51c30f2c6eb81995f55beb28300e4eb32
Lachlan Vass <lachlan.vass@okendo.io>
2023-11-17 11:20:38 +1000

    use val for param

--- app/src/main/java/deakin/gopher/guardian/model/login/RoleName.kt
+++ app/src/main/java/deakin/gopher/guardian/model/login/RoleName.kt
@@ -2,7 +2,7 @@ package deakin.gopher.guardian.model.login
 
 import deakin.gopher.guardian.R
 
-sealed class RoleName(name: String) {
+sealed class RoleName(val name: String) {
     data object Caretaker : RoleName(R.string.caretaker_role_name.toString())
     data object Admin : RoleName(R.string.company_admin_role_name.toString())
     data object Nurse : RoleName(R.string.nurse_role_name.toString())
