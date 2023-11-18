b7d67ba07a44e5994538dbb8bd566d942c658ef3
waqarsoomro9714 <101506898+waqarsoomro9714@users.noreply.github.com>
2023-11-19 07:29:41 +1100

    Migrate Medical_diagnostic Model to Kotlin
    Migrate Medical_diagnostic Model to Kotlin

--- app/src/main/java/deakin/gopher/guardian/model/MedicalDiagnostic.kt
+++ app/src/main/java/deakin/gopher/guardian/model/MedicalDiagnostic.kt
@@ -1,15 +1,22 @@
 package deakin.gopher.guardian.model
 
 data class MedicalDiagnostic(
-    val patientId: String,
-    val name: String? = null,
-    val bloodPressure: String? = null,
-    val patientTemp: String? = null,
-    val glucoseLevel: String? = null,
-    val oxygenSaturation: String? = null,
-    val pulseRate: String? = null,
-    val respirationRate: String? = null,
-    val bloodFatLevel: String? = null,
-    val current: Boolean
-)
+    var patientId: String? = null,
+    var name: String? = null,
+    var bloodPressure: String? = null,
+    var patientTemp: String? = null,
+    var glucoseLevel: String? = null,
+    var oxygenSaturation: String? = null,
+    var pulseRate: String? = null,
+    var respirationRate: String? = null,
+    var bloodFatLevel: String? = null,
+    var current: Boolean? = null
+) {
+    constructor(patient_id: String, isCurrent: Boolean) : this()  {
+        patientId = patient_id
+        current = isCurrent
+
+
+    }
+}
 
