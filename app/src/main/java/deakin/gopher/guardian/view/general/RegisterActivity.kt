46485af4705708af5253e9a782cdda37f3292615
Lachlan Vass <lachlan.vass@okendo.io>
2023-11-20 20:42:26 +1000

    convert to Kotlin

--- app/src/main/java/deakin/gopher/guardian/view/general/RegisterActivity.kt
+++ app/src/main/java/deakin/gopher/guardian/view/general/RegisterActivity.kt
@@ -1,129 +1,140 @@
-package deakin.gopher.guardian.view.general;
+package deakin.gopher.guardian.view.general
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import deakin.gopher.guardian.R;
-import java.util.HashMap;
-import java.util.Map;
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import deakin.gopher.guardian.R
 
-public class RegisterActivity extends AppCompatActivity {
-  public static final String TAG = "TAG";
-  EditText mEmail, mPassword;
-  Button mRegisterBtn;
-  TextView mLoginBtn;
-  FirebaseAuth Auth;
-  ProgressBar progressBar;
-  FirebaseFirestore fStore;
-  String userID;
+class RegisterActivity : AppCompatActivity() {
+    var mEmail: EditText? = null
+    var mPassword: EditText? = null
+    var mRegisterBtn: Button? = null
+    var mLoginBtn: TextView? = null
+    var Auth: FirebaseAuth? = null
+    var progressBar: ProgressBar? = null
+    var fStore: FirebaseFirestore? = null
+    var userID: String? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        val backToLoginButton = findViewById<Button>(R.id.backToLoginButton)
+        backToLoginButton.setOnClickListener { view: View? ->
+            val loginIntent = Intent(this@RegisterActivity, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
+        val mEmail: EditText = findViewById(R.id.Email)
+        val mPassword: EditText = findViewById(R.id.password)
+        val mRegisterBtn: Button = findViewById(R.id.registerBtn)
+        val mLoginBtn: Button = findViewById(R.id.loginRegisterBtn)
 
-  @Override
-  protected void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_register);
 
-    final Button backToLoginButton = findViewById(R.id.backToLoginButton);
+        val Auth = FirebaseAuth.getInstance()
+        val fStore = FirebaseFirestore.getInstance()
 
-    backToLoginButton.setOnClickListener(
-        view -> {
-          final Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
-          startActivity(loginIntent);
-        });
 
-    mEmail = findViewById(R.id.Email);
-    mPassword = findViewById(R.id.password);
-    mRegisterBtn = findViewById(R.id.registerBtn);
-    mLoginBtn = findViewById(R.id.loginRegisterBtn);
+        val progressBar = findViewById(R.id.progressBar)
+        mRegisterBtn.setOnClickListener { v: View? ->
+            val email = mEmail.text.toString().trim { it <= ' ' }
+            val password = mPassword.text.toString().trim { it <= ' ' }
+            if (TextUtils.isEmpty(email)) {
+                mEmail.error = "Email is Required."
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)) {
+                mPassword.error = "Password is Required."
+                return@setOnClickListener
+            }
+            if (6 > password.length) {
+                mPassword.error = "Password Must be >= 6 Characters"
+                return@setOnClickListener
+            }
+            progressBar.setVisibility(View.VISIBLE)
 
-    Auth = FirebaseAuth.getInstance();
-    fStore = FirebaseFirestore.getInstance();
-    progressBar = findViewById(R.id.progressBar);
+            // register the user in firebase
+            Auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task: Task<AuthResult?> ->
+                    if (task.isSuccessful) {
 
-    mRegisterBtn.setOnClickListener(
-        v -> {
-          final String email = mEmail.getText().toString().trim();
-          final String password = mPassword.getText().toString().trim();
-
-          if (TextUtils.isEmpty(email)) {
-            mEmail.setError("Email is Required.");
-            return;
-          }
-
-          if (TextUtils.isEmpty(password)) {
-            mPassword.setError("Password is Required.");
-            return;
-          }
-
-          if (6 > password.length()) {
-            mPassword.setError("Password Must be >= 6 Characters");
-            return;
-          }
-
-          progressBar.setVisibility(View.VISIBLE);
-
-          // register the user in firebase
-
-          Auth.createUserWithEmailAndPassword(email, password)
-              .addOnCompleteListener(
-                  task -> {
-                    if (task.isSuccessful()) {
-
-                      // send verification link
-
-                      final FirebaseUser fuser = Auth.getCurrentUser();
-                      fuser
-                          .sendEmailVerification()
-                          .addOnSuccessListener(
-                              aVoid ->
-                                  Toast.makeText(
-                                          RegisterActivity.this,
-                                          "Verification Email Has been Sent.",
-                                          Toast.LENGTH_SHORT)
-                                      .show())
-                          .addOnFailureListener(
-                              e -> Log.d(TAG, "onFailure: Email not sent " + e.getMessage()));
-
-                      Toast.makeText(RegisterActivity.this, "User Created.", Toast.LENGTH_SHORT)
-                          .show();
-                      userID = Auth.getCurrentUser().getUid();
-                      final DocumentReference documentReference =
-                          fStore.collection("users").document(userID);
-                      final Map<String, Object> user = new HashMap<>();
-
-                      user.put("email", email);
-
-                      documentReference
-                          .set(user)
-                          .addOnSuccessListener(
-                              aVoid ->
-                                  Log.d(TAG, "onSuccess: user Profile is created for " + userID))
-                          .addOnFailureListener(e -> Log.d(TAG, "onFailure: " + e));
-                      startActivity(new Intent(getApplicationContext(), MainActivity.class));
-
+                        // send verification link
+                        val fuser = Auth.currentUser
+                        fuser
+                            .sendEmailVerification()
+                            .addOnSuccessListener { aVoid: Void? ->
+                                Toast.makeText(
+                                    this@RegisterActivity,
+                                    "Verification Email Has been Sent.",
+                                    Toast.LENGTH_SHORT
+                                )
+                                    .show()
+                            }
+                            .addOnFailureListener { e: Exception ->
+                                Log.d(
+                                    TAG,
+                                    "onFailure: Email not sent " + e.message
+                                )
+                            }
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "User Created.",
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        userID = Auth.currentUser!!.uid
+                        val documentReference = fStore.collection("users").document(
+                            userID!!
+                        )
+                        val user: MutableMap<String, Any> = HashMap()
+                        user["email"] = email
+                        documentReference
+                            .set(user)
+                            .addOnSuccessListener { aVoid: Void? ->
+                                Log.d(
+                                    TAG,
+                                    "onSuccess: user Profile is created for $userID"
+                                )
+                            }
+                            .addOnFailureListener { e: Exception ->
+                                Log.d(
+                                    TAG,
+                                    "onFailure: $e"
+                                )
+                            }
+                        startActivity(Intent(applicationContext, MainActivity::class.java))
                     } else {
-                      Toast.makeText(
-                              RegisterActivity.this,
-                              "Error ! " + task.getException().getMessage(),
-                              Toast.LENGTH_SHORT)
-                          .show();
-                      progressBar.setVisibility(View.GONE);
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "Error ! " + task.exception!!.message,
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                        progressBar.setVisibility(View.GONE)
                     }
-                  });
-        });
+                }
+        }
+        
+        mLoginBtn.setOnClickListener(
+            View.OnClickListener { v: View? ->
+                startActivity(
+                    Intent(
+                        applicationContext, LoginActivity::class.java
+                    )
+                )
+            })
+    }
 
-    mLoginBtn.setOnClickListener(
-        v -> startActivity(new Intent(getApplicationContext(), LoginActivity.class)));
-  }
+    companion object {
+        const val TAG = "TAG"
+    }
 }
