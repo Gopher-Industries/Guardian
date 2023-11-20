695698750fcb041f1e8ce6cf1cec953ed507dc69
Lachlan Vass <lachlan.vass@okendo.io>
2023-11-20 20:42:46 +1000

    create registration errors

---
+++ app/src/main/java/deakin/gopher/guardian/model/register/RegistrationError.kt
@@ -0,0 +1,16 @@
+package deakin.gopher.guardian.model.register
+
+import deakin.gopher.guardian.R
+
+sealed class RegistrationError(val message: String) {
+    data object EmptyEmail : RegistrationError(R.string.validation_empty_email.toString())
+    data object InvalidEmail :
+        RegistrationError(R.string.validation_invalid_email_address.toString())
+
+    data object EmptyPassword : RegistrationError(R.string.validation_empty_password.toString())
+    data object PasswordTooShort :
+        RegistrationError(R.string.validation_password_short.toString())
+
+    data object PasswordsFailConfirmation :
+        RegistrationError(R.string.validation_error_passwords_do_not_match.toString())
+}
