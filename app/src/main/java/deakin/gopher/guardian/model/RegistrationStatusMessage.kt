7867b947720ebebe0d7f6bd94c338255a7543ee9
Lachlan Vass <lachlan.vass@okendo.io>
2023-11-21 11:55:16 +1000

    add RegistrationStatusMessage.kt

---
+++ app/src/main/java/deakin/gopher/guardian/model/RegistrationStatusMessage.kt
@@ -0,0 +1,8 @@
+package deakin.gopher.guardian.model
+
+import deakin.gopher.guardian.R
+
+sealed class RegistrationStatusMessage(val message: Int) {
+    data object Success : RegistrationStatusMessage(R.string.registration_success)
+    data object Failure : RegistrationStatusMessage(R.string.registration_failure)
+}
