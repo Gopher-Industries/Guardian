5fb5e627b904ab273b2eaaaba33c34de893502ac
waqarsoomro9714 <101506898+waqarsoomro9714@users.noreply.github.com>
2023-11-29 07:50:09 +1100

    sidebar

--- app/src/main/java/deakin/gopher/guardian/view/general/PatientProfileActivity.java
+++ app/src/main/java/deakin/gopher/guardian/view/general/PatientProfileActivity.java
@@ -1,5 +1,6 @@
 package deakin.gopher.guardian.view.general;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.core.view.GravityCompat;
@@ -8,99 +9,126 @@ import androidx.viewpager2.widget.ViewPager2;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseAuth;
+
 import deakin.gopher.guardian.R;
 import deakin.gopher.guardian.adapter.PatientProfileAdapter;
 
 public class PatientProfileActivity extends BaseActivity {
 
-  private CustomHeader customHeader;
+    private CustomHeader customHeader;
 
-  @Override
-  protected void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_patient_profile);
+    private DrawerLayout drawerLayout;
 
-    final TabLayout tabLayout = findViewById(R.id.dataForViewTabLayout);
-    final ViewPager2 viewPager2 = findViewById(R.id.dataForViewViewPager);
-    customHeader = findViewById(R.id.customHeader);
-    final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-    final NavigationView navigationView = findViewById(R.id.nav_view);
-    final String patient_id = getIntent().getStringExtra("id");
+    private FirebaseAuth mAuth;
 
-    final PatientProfileAdapter viewPagerAdapter =
-        new PatientProfileAdapter(patient_id, getSupportFragmentManager(), getLifecycle());
-    viewPager2.setAdapter(viewPagerAdapter);
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_profile);
 
-    customHeader.setHeaderHeight(450);
-    customHeader.setHeaderText("Patient Profile");
-    customHeader.setHeaderTopImageVisibility(View.VISIBLE);
+        final TabLayout tabLayout = findViewById(R.id.dataForViewTabLayout);
+        final ViewPager2 viewPager2 = findViewById(R.id.dataForViewViewPager);
+        customHeader = findViewById(R.id.customHeader);
+        drawerLayout = findViewById(R.id.nav_drawer_layout);
+        final NavigationView navigationView = findViewById(R.id.nav_view);
+        final String patient_id = getIntent().getStringExtra("id");
 
-    navigationView.setItemIconTintList(null);
+        final PatientProfileAdapter viewPagerAdapter =
+                new PatientProfileAdapter(patient_id, getSupportFragmentManager(), getLifecycle());
+        viewPager2.setAdapter(viewPagerAdapter);
 
-    if (null != customHeader) {
-      customHeader.menuButton.setOnClickListener(
-          v -> {
-            if (null != drawerLayout) {
-              drawerLayout.openDrawer(GravityCompat.START);
-            }
-          });
+        customHeader.setHeaderHeight(450);
+        customHeader.setHeaderText("Patient Profile");
+        customHeader.setHeaderTopImageVisibility(View.VISIBLE);
+
+        navigationView.setItemIconTintList(null);
+
+        if (null != customHeader) {
+            customHeader.menuButton.setOnClickListener(
+                    v -> {
+                        if (null != drawerLayout) {
+                            drawerLayout.openDrawer(GravityCompat.START);
+
+                            navigationView.setNavigationItemSelectedListener(
+                                    menuItem -> {
+                                        final int id = menuItem.getItemId();
+                                        if (R.id.nav_home == id) {
+                                            startActivity(new Intent(PatientProfileActivity.this, Homepage4admin.class));
+                                        } else if (R.id.nav_admin == id) {
+                                            startActivity(new Intent(PatientProfileActivity.this, Homepage4admin.class));
+                                        } else if (R.id.nav_settings == id) {
+                                            startActivity(new Intent(PatientProfileActivity.this, Setting.class));
+                                        } else if (R.id.nav_signout == id) {
+                                            mAuth.getInstance()
+                                                    .signOut();
+                                            startActivity(new Intent(PatientProfileActivity.this, LoginActivity.class));
+                                            finish();
+
+                                        }
+                                        return true;
+                                    });
+
+
+                        }
+                    });
+        }
+
+        new TabLayoutMediator(
+                tabLayout,
+                viewPager2,
+                (tab, position) -> {
+                    if (0 == position) {
+                        tab.setText("Patient");
+                    } else if (1 == position) {
+                        tab.setText("Next of Kin");
+                    } else if (2 == position) {
+                        tab.setText("General Practitioner");
+                    } else if (3 == position) {
+                        tab.setText("Health Details");
+                    } else if (4 == position) {
+                        tab.setText("Health & Welfare Det.");
+                    } else {
+                        tab.setText("Care Plan");
+                    }
+                })
+                .attach();
+
+        tabLayout.addOnTabSelectedListener(
+                new TabLayout.OnTabSelectedListener() {
+                    @Override
+                    public void onTabSelected(final TabLayout.Tab tab) {
+                        final int position = tab.getPosition();
+                        if (0 == position) {
+                            customHeader.setHeaderText("Patient Profile");
+                            customHeader.setHeaderTopImageVisibility(View.VISIBLE);
+                            customHeader.setHeaderTopImage(R.drawable.profile_avatar_women);
+                        } else if (1 == position) {
+                            customHeader.setHeaderText("Next Of Kin Contact");
+                            customHeader.setHeaderTopImage(R.drawable.profile_avatar_men);
+                            customHeader.setHeaderTopImageVisibility(View.VISIBLE);
+                        } else if (2 == position) {
+                            customHeader.setHeaderText("GP Details");
+                            customHeader.setHeaderTopImage(R.drawable.profile_avatar_men2);
+                            customHeader.setHeaderTopImageVisibility(View.VISIBLE);
+                        } else if (3 == position) {
+                            customHeader.setHeaderText("Health Details");
+                            customHeader.setHeaderTopImageVisibility(View.GONE);
+                        } else if (4 == position) {
+                            customHeader.setHeaderText("Patient Details");
+                            customHeader.setHeaderTopImageVisibility(View.GONE);
+                        } else {
+                            customHeader.setHeaderText("Care Plan");
+                            customHeader.setHeaderTopImage(R.drawable.profile_avatar_men);
+                            customHeader.setHeaderTopImageVisibility(View.VISIBLE);
+                        }
+                    }
+
+                    @Override
+                    public void onTabUnselected(final TabLayout.Tab tab) {}
+
+                    @Override
+                    public void onTabReselected(final TabLayout.Tab tab) {}
+                });
     }
-
-    new TabLayoutMediator(
-            tabLayout,
-            viewPager2,
-            (tab, position) -> {
-              if (0 == position) {
-                tab.setText("Patient");
-              } else if (1 == position) {
-                tab.setText("Next of Kin");
-              } else if (2 == position) {
-                tab.setText("General Practitioner");
-              } else if (3 == position) {
-                tab.setText("Health Details");
-              } else if (4 == position) {
-                tab.setText("Health & Welfare Det.");
-              } else {
-                tab.setText("Care Plan");
-              }
-            })
-        .attach();
-
-    tabLayout.addOnTabSelectedListener(
-        new TabLayout.OnTabSelectedListener() {
-          @Override
-          public void onTabSelected(final TabLayout.Tab tab) {
-            final int position = tab.getPosition();
-            if (0 == position) {
-              customHeader.setHeaderText("Patient Profile");
-              customHeader.setHeaderTopImageVisibility(View.VISIBLE);
-              customHeader.setHeaderTopImage(R.drawable.profile_avatar_women);
-            } else if (1 == position) {
-              customHeader.setHeaderText("Next Of Kin Contact");
-              customHeader.setHeaderTopImage(R.drawable.profile_avatar_men);
-              customHeader.setHeaderTopImageVisibility(View.VISIBLE);
-            } else if (2 == position) {
-              customHeader.setHeaderText("GP Details");
-              customHeader.setHeaderTopImage(R.drawable.profile_avatar_men2);
-              customHeader.setHeaderTopImageVisibility(View.VISIBLE);
-            } else if (3 == position) {
-              customHeader.setHeaderText("Health Details");
-              customHeader.setHeaderTopImageVisibility(View.GONE);
-            } else if (4 == position) {
-              customHeader.setHeaderText("Patient Details");
-              customHeader.setHeaderTopImageVisibility(View.GONE);
-            } else {
-              customHeader.setHeaderText("Care Plan");
-              customHeader.setHeaderTopImage(R.drawable.profile_avatar_men);
-              customHeader.setHeaderTopImageVisibility(View.VISIBLE);
-            }
-          }
-
-          @Override
-          public void onTabUnselected(final TabLayout.Tab tab) {}
-
-          @Override
-          public void onTabReselected(final TabLayout.Tab tab) {}
-        });
-  }
 }
