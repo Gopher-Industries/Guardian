3716e460dfa925b652e6bd82a33b2237713ccc6a
Lachlan Vass <lachlan.vass@okendo.io>
2023-12-03 16:47:59 +1000

    add more espresso packages and instrumented UI test

---
+++ app/src/androidTest/java/view/general/LoginActivityInstrumentedTest.kt
@@ -0,0 +1,50 @@
+package view.general
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.RootMatchers.isPlatformPopup
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import deakin.gopher.guardian.R
+import deakin.gopher.guardian.view.general.LoginActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class LoginActivityInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(LoginActivity::class.java)
+
+    @Test
+    fun onLoginAttempt_LoginFailsWithNoCredentials() {
+        onView(withId(R.id.loginBtn)).perform(click())
+        intended(hasComponent(LoginActivity::class.java.name))
+    }
+
+    @Test
+    fun onLoginAttempt_LoginFailsWithBadEmailAddress() {
+        onView(withId(R.id.Email)).perform(typeText("BadEmailAddress"))
+        onView(withId(R.id.loginBtn)).perform(click())
+
+        onView(withText(R.string.validation_invalid_email_address))
+            .inRoot(isPlatformPopup())
+            .check(matches(isDisplayed()))
+    }
+
+}
