448829edb19ec95b538372dbedcdc3b83c954d0c
Hugo Smith <smithhug@deakin.edu.au>
2023-12-06 20:56:38 +1100

    Created test data
    Firebase could not be implemented due to time constrants

--- app/src/main/java/deakin/gopher/guardian/view/general/TasksListActivity.kt
+++ app/src/main/java/deakin/gopher/guardian/view/general/TasksListActivity.kt
@@ -1,4 +1,4 @@
-package deakin.gopher.guardian.view.general
+package deakin.gopher.guardian.view.general;
 
 import android.os.Bundle
 import android.widget.Button
@@ -8,24 +8,22 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.database.FirebaseRecyclerOptions
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.Query
-import com.google.firebase.database.ValueEventListener
 import deakin.gopher.guardian.R
 import deakin.gopher.guardian.adapter.TaskListAdapter
 import deakin.gopher.guardian.model.Task
 import deakin.gopher.guardian.services.NavigationService
 
 class TasksListActivity : AppCompatActivity() {
-    var taskListAdapter: TaskListAdapter? = null
-    var query: Query? = null
-    var overviewCardview: CardView? = null
+    private var taskListAdapter: TaskListAdapter? = null
+    private var query: Query? = null
+    private var overviewCardview: CardView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks_list)
+
         val taskListRecyclerView: RecyclerView = findViewById(R.id.task_list_recycleView)
         overviewCardview = findViewById(R.id.task_list_task_overview)
         val taskSearchView: SearchView = findViewById(R.id.task_list_searchView)
@@ -37,15 +35,9 @@ class TasksListActivity : AppCompatActivity() {
 
         query = FirebaseDatabase.getInstance().reference.child("tasks")
 
-        val allOptions =
-            FirebaseRecyclerOptions.Builder<Task>()
-                .setQuery(query!!, Task::class.java)
-                .build()
-
-        val taskListAdapterDefault = TaskListAdapter(this@TasksListActivity, allOptions)
+        taskListAdapter = TaskListAdapter(getTestData())
         taskListRecyclerView.layoutManager = GridLayoutManager(this@TasksListActivity, 1)
-        taskListRecyclerView.adapter = taskListAdapterDefault
-        taskListAdapterDefault.startListening()
+        taskListRecyclerView.adapter = taskListAdapter
 
         taskSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(s: String?): Boolean {
@@ -65,33 +57,15 @@ class TasksListActivity : AppCompatActivity() {
                         .limitToFirst(10)
                 }
 
-                val options = FirebaseRecyclerOptions.Builder<Task>()
-                    .setQuery(query!!, Task::class.java)
-                    .build()
-
-                taskListAdapter = TaskListAdapter(this@TasksListActivity, options)
-                query!!.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        if (snapshot.exists()) {
-                            taskListAdapter = TaskListAdapter(this@TasksListActivity, options)
-                            taskListRecyclerView.layoutManager =
-                                GridLayoutManager(this@TasksListActivity, 1)
-                            taskListRecyclerView.adapter = taskListAdapter
-                            taskListAdapter!!.startListening()
-                        } else {
-                            taskListRecyclerView.layoutManager =
-                                GridLayoutManager(this@TasksListActivity, 1)
-                            taskListRecyclerView.adapter =
-                                TaskListAdapter(this@TasksListActivity, options)
-                        }
-                    }
-
-                    override fun onCancelled(error: DatabaseError) {}
-                })
-
                 return true
-                }
-            },
+            }
+        })
+    }
+
+    private fun getTestData(): List<Task> {
+        return listOf(
+            Task("1", "Task 1 Description", "Patient 1"),
+            Task("2", "Task 2 Description", "Patient 2"),
         )
     }
 }
