fd4961074a6c0c63c8bb0a59027407889bac3744
sicsix <8847273+sicsix@users.noreply.github.com>
2023-12-10 15:39:29 +1100

    - Converted MedicalDiagnosticsViewPagerAdapter.java and PatientProfileAddAdapter.java to Kotlin

--- app/src/main/java/deakin/gopher/guardian/adapter/MedicalDiagnosticsViewPagerAdapter.kt
+++ app/src/main/java/deakin/gopher/guardian/adapter/MedicalDiagnosticsViewPagerAdapter.kt
@@ -1,36 +1,29 @@
-package deakin.gopher.guardian.adapter;
+package deakin.gopher.guardian.adapter
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import deakin.gopher.guardian.view.patient.patientdata.medicaldiagnostics.CurrentMedicalDiagnosticsFragment;
-import deakin.gopher.guardian.view.patient.patientdata.medicaldiagnostics.MedicalDiagnosticsFragment;
-import deakin.gopher.guardian.view.patient.patientdata.medicaldiagnostics.PastMedicalDiagnosticsFragment;
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import deakin.gopher.guardian.view.patient.patientdata.medicaldiagnostics.CurrentMedicalDiagnosticsFragment
+import deakin.gopher.guardian.view.patient.patientdata.medicaldiagnostics.MedicalDiagnosticsFragment
+import deakin.gopher.guardian.view.patient.patientdata.medicaldiagnostics.PastMedicalDiagnosticsFragment
 
-public class MedicalDiagnosticsViewPagerAdapter extends FragmentStateAdapter {
-  private final MedicalDiagnosticsFragment parentFragment;
-  private final String patient_id;
-
-  public MedicalDiagnosticsViewPagerAdapter(
-      final String patient_id, @NonNull final MedicalDiagnosticsFragment fragmentActivity) {
-    super(fragmentActivity);
-    parentFragment = fragmentActivity;
-    this.patient_id = patient_id;
-  }
-
-  @NonNull
-  @Override
-  public Fragment createFragment(final int position) {
-    if (0 == position) {
-      parentFragment.currentFragment = new CurrentMedicalDiagnosticsFragment(patient_id);
-      return parentFragment.currentFragment;
+class MedicalDiagnosticsViewPagerAdapter(
+    private val patientId: String,
+    private val parentFragment: MedicalDiagnosticsFragment,
+) : FragmentStateAdapter(
+        parentFragment,
+    ) {
+    override fun createFragment(position: Int): Fragment {
+        if (position == 0) {
+            val currentFragment = CurrentMedicalDiagnosticsFragment(patientId)
+            parentFragment.currentFragment = currentFragment
+            return currentFragment
+        }
+        val pastFragment = PastMedicalDiagnosticsFragment(patientId)
+        parentFragment.pastFragment = pastFragment
+        return pastFragment
     }
-    parentFragment.pastFragment = new PastMedicalDiagnosticsFragment(patient_id);
-    return parentFragment.pastFragment;
-  }
 
-  @Override
-  public int getItemCount() {
-    return 2;
-  }
+    override fun getItemCount(): Int {
+        return 2
+    }
 }
