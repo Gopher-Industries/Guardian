796454fe3a025e5d08a9cf6ba096db02de4fa323
sicsix <8847273+sicsix@users.noreply.github.com>
2023-12-10 16:32:40 +1100

    - Removed button at top of health details - Added arrow buttons to both current and past screens - Converted CurrentMedicalDiagnosticsFragment.java, MedicalDiagnosticsActivity.java, and PastMedicalDiagnosticsFragment.java to Kotlin

--- app/src/main/java/deakin/gopher/guardian/view/patient/patientdata/medicaldiagnostics/PastMedicalDiagnosticsFragment.kt
+++ app/src/main/java/deakin/gopher/guardian/view/patient/patientdata/medicaldiagnostics/PastMedicalDiagnosticsFragment.kt
@@ -1,161 +1,164 @@
-package deakin.gopher.guardian.view.patient.patientdata.medicaldiagnostics;
+package deakin.gopher.guardian.view.patient.patientdata.medicaldiagnostics
 
-import android.content.Context;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-import deakin.gopher.guardian.R;
-import deakin.gopher.guardian.model.MedicalDiagnostic;
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import deakin.gopher.guardian.R
+import deakin.gopher.guardian.model.MedicalDiagnostic
+import deakin.gopher.guardian.view.patient.patientdata.heartrate.HeartRateActivity
 
-public class PastMedicalDiagnosticsFragment extends Fragment {
+class PastMedicalDiagnosticsFragment : Fragment {
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private val editTextIds =
+        intArrayOf(
+            R.id.pastMedicalDiagnosticsNameTextView,
+            R.id.pastMedicalDiagnosticsBloodPressureTextView,
+            R.id.pastMedicalDiagnosticsHeartRateTextView,
+            R.id.pastMedicalDiagnosticsPatientTemperatureTextView,
+            R.id.pastMedicalDiagnosticsGlucoseLevelTextView,
+            R.id.pastMedicalDiagnosticsO2SaturationTextView,
+            R.id.pastMedicalDiagnosticsPulseRateTextView,
+            R.id.pastMedicalDiagnosticsRespirationRateTextView,
+            R.id.pastMedicalDiagnosticsBloodfatLevelTextView,
+        )
+    private val editButtonIds =
+        intArrayOf(
+            R.id.past_name_pencil,
+            R.id.past_blood_pressure_pencil,
+            R.id.past_heart_rate_pencil,
+            R.id.past_temperature_pencil,
+            R.id.past_glucose_level_pencil,
+            R.id.past_blood_o2_saturation_pencil,
+            R.id.past_pulse_rate_pencil,
+            R.id.past_respiration_rate_pencil,
+            R.id.past_bloodfat_level_pencil,
+        )
 
-  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-  private final int[] editTextIds = {
-    R.id.pastMedicalDiagnosticsNameTextView,
-    R.id.pastMedicalDiagnosticsBloodPressureTextView,
-    R.id.pastMedicalDiagnosticsPatientTemperatureTextView,
-    R.id.pastMedicalDiagnosticsGlucoseLevelTextView,
-    R.id.pastMedicalDiagnosticsO2SaturationTextView,
-    R.id.pastMedicalDiagnosticsPulseRateTextView,
-    R.id.pastMedicalDiagnosticsRespirationRateTextView,
-    R.id.pastMedicalDiagnosticsBloodfatLevelTextView
-  };
+    private lateinit var medicalDiagnosticPast: MedicalDiagnostic
+    private lateinit var editTextArray: Array<EditText?>
+    private lateinit var editButtonArray: Array<Button?>
+    private lateinit var patientId: String
 
-  private final int[] editButtonIds = {
-    R.id.past_name_pencil,
-    R.id.past_blood_pressure_pencil,
-    R.id.past_temperature_pencil,
-    R.id.past_glucose_level_pencil,
-    R.id.past_blood_o2_saturation_pencil,
-    R.id.past_pulse_rate_pencil,
-    R.id.past_respiration_rate_pencil,
-    R.id.past_bloodfat_level_pencil
-  };
-  MedicalDiagnostic medical_diagnostic_past;
-  private EditText[] editTextArray;
-  private Button[] editButtonArray;
-  private String patientId;
-
-  public PastMedicalDiagnosticsFragment() {
-    // Required empty public constructor
-  }
-
-  public PastMedicalDiagnosticsFragment(final String patientId) {
-    this.patientId = patientId;
-  }
-
-  @Override
-  public void onCreate(final Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-  }
-
-  @Override
-  public View onCreateView(
-      final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
-    // Inflate the layout for this fragment
-    final View rootView =
-        inflater.inflate(R.layout.fragment_past_medical_diagnostics, container, false);
-    editTextArray = new EditText[editTextIds.length];
-    editButtonArray = new Button[editButtonIds.length];
-
-    for (int i = 0; i < editTextIds.length; i++) {
-      editTextArray[i] = rootView.findViewById(editTextIds[i]);
-      editButtonArray[i] = rootView.findViewById(editButtonIds[i]);
-
-      final EditText editText = editTextArray[i];
-      final Button editButton = editButtonArray[i];
-
-      editButton.setOnClickListener(
-          v -> {
-            editText.setFocusable(true);
-            editText.setFocusableInTouchMode(true);
-            editText.setEnabled(true);
-            editText.requestFocus();
-            editText.selectAll();
-            final InputMethodManager imm =
-                (InputMethodManager)
-                    requireContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
-          });
-    }
-
-    return rootView;
-  }
-
-  public void setEditState(final boolean isEditable) {
-    if (isEditable) {
-      for (final Button editButton : editButtonArray) {
-        editButton.setVisibility(View.VISIBLE);
-      }
-    } else {
-      for (final Button editButton : editButtonArray) {
-        editButton.setVisibility(View.INVISIBLE);
-      }
-      for (final EditText editText : editTextArray) {
-        editText.setFocusable(false);
-        editText.setFocusableInTouchMode(false);
-        editText.setEnabled(false);
-      }
-      saveInFirebase();
-    }
-  }
-
-  private void saveInFirebase() {
-    final DatabaseReference reference =
-        FirebaseDatabase.getInstance().getReference("health_details");
-    final Query query = reference.orderByChild("patient_id").equalTo(patientId);
-    if (dataChecker()) {
-      query.addListenerForSingleValueEvent(new SaveInFirebaseListener());
-    }
-  }
-
-  private Boolean dataChecker() {
-    for (final EditText editText : editTextArray) {
-      if (TextUtils.isEmpty(editText.getText())) {
-        editText.setError("it shouldn't be empty!");
-        return false;
-      }
-    }
-    medical_diagnostic_past =
-        new MedicalDiagnostic(
-            patientId,
-            editTextArray[0].getText().toString(),
-            editTextArray[1].getText().toString(),
-            editTextArray[2].getText().toString(),
-            editTextArray[3].getText().toString(),
-            editTextArray[4].getText().toString(),
-            editTextArray[5].getText().toString(),
-            editTextArray[6].getText().toString(),
-            editTextArray[7].getText().toString(),
-            false);
-    return true;
-  }
-
-  private class SaveInFirebaseListener implements ValueEventListener {
-    @Override
-    public void onDataChange(@NonNull final DataSnapshot snapshot) {
-      for (final DataSnapshot childSnapshot : snapshot.getChildren()) {
-        final Boolean isCurrent = childSnapshot.child("current").getValue(Boolean.class);
-        if (Boolean.FALSE.equals(isCurrent)) {
-          childSnapshot.getRef().setValue(medical_diagnostic_past);
+    constructor()
+    constructor(patientId: String?) {
+        if (patientId != null) {
+            this.patientId = patientId
         }
-      }
     }
 
-    @Override
-    public void onCancelled(@NonNull final DatabaseError error) {}
-  }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        val rootView =
+            inflater.inflate(R.layout.fragment_past_medical_diagnostics, container, false)
+        editTextArray = arrayOfNulls(editTextIds.size)
+        editButtonArray = arrayOfNulls(editButtonIds.size)
+        for (i in editTextIds.indices) {
+            editTextArray[i] = rootView.findViewById(editTextIds[i])
+            editButtonArray[i] = rootView.findViewById(editButtonIds[i])
+            val editText = editTextArray[i]
+            val editButton = editButtonArray[i]
+            editButton!!.setOnClickListener { v: View? ->
+                editText!!.isFocusable = true
+                editText.isFocusableInTouchMode = true
+                editText.isEnabled = true
+                editText.requestFocus()
+                editText.selectAll()
+                val imm =
+                    requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+            }
+        }
+
+        val heartRateArrowButton: ImageView =
+            rootView.findViewById(R.id.pastMedicalDiagnosticsHeartRateArrowButton)
+        heartRateArrowButton.setOnClickListener {
+            Intent(this.context, HeartRateActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
+        return rootView
+    }
+
+    fun setEditState(isEditable: Boolean) {
+        if (isEditable) {
+            for (editButton in editButtonArray) {
+                editButton!!.visibility = View.VISIBLE
+            }
+        } else {
+            for (editButton in editButtonArray) {
+                editButton!!.visibility = View.INVISIBLE
+            }
+            for (editText in editTextArray) {
+                editText!!.isFocusable = false
+                editText.isFocusableInTouchMode = false
+                editText.isEnabled = false
+            }
+            saveInFirebase()
+        }
+    }
+
+    private fun saveInFirebase() {
+        val reference = FirebaseDatabase.getInstance().getReference("health_details")
+        val query = reference.orderByChild("patient_id").equalTo(patientId)
+        if (dataChecker()) {
+            query.addListenerForSingleValueEvent(SaveInFirebaseListener())
+        }
+    }
+
+    private fun dataChecker(): Boolean {
+        for (editText in editTextArray) {
+            if (TextUtils.isEmpty(editText!!.text)) {
+                editText.error = "it shouldn't be empty!"
+                return false
+            }
+        }
+        medicalDiagnosticPast =
+            MedicalDiagnostic(
+                patientId,
+                editTextArray[0]!!.text.toString(),
+                editTextArray[1]!!.text.toString(),
+                editTextArray[2]!!.text.toString(),
+                editTextArray[3]!!.text.toString(),
+                editTextArray[4]!!.text.toString(),
+                editTextArray[5]!!.text.toString(),
+                editTextArray[6]!!.text.toString(),
+                editTextArray[7]!!.text.toString(),
+                false,
+            )
+        return true
+    }
+
+    private inner class SaveInFirebaseListener : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            for (childSnapshot in snapshot.children) {
+                val isCurrent =
+                    childSnapshot.child("current").getValue(
+                        Boolean::class.java,
+                    )
+                if (java.lang.Boolean.FALSE == isCurrent) {
+                    childSnapshot.ref.setValue(medicalDiagnosticPast)
+                }
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {}
+    }
 }
