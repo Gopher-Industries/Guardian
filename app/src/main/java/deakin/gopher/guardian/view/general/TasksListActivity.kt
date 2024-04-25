cb09a59b5f7264953a7478f3b0c57fcac72c9df9
Dhanuka Abeywardhane <dhabeywa@yvw.com.au>
2024-04-25 23:50:48 +1000

    Added side menu on activity task list
    Trello card : https://trello.com/c/QYm4K9U7/178-add-side-menu-on-activity-task-list

--- app/src/main/java/deakin/gopher/guardian/view/general/TasksListActivity.kt
+++ app/src/main/java/deakin/gopher/guardian/view/general/TasksListActivity.kt
@@ -33,6 +33,7 @@ class TasksListActivity : AppCompatActivity() {
         var taskListMenuBtn : ImageView = findViewById(R.id.task_list_manu_button);
         var drawerLayout : DrawerLayout = findViewById(R.id.drawer_layout);
 
+        //test
         navigationView.setItemIconTintList(null);
         taskListMenuBtn.setOnClickListener({
             drawerLayout.openDrawer(GravityCompat.START);
