aa679665fc606169cf55ce99c30df436104240c6
Dhanuka Abeywardhane <dhabeywa@yvw.com.au>
2024-04-25 23:39:23 +1000

    Added side menu on activity task list
    Trello card : https://trello.com/c/QYm4K9U7/178-add-side-menu-on-activity-task-list

--- app/src/main/java/deakin/gopher/guardian/view/general/TasksListActivity.kt
+++ app/src/main/java/deakin/gopher/guardian/view/general/TasksListActivity.kt
@@ -1,22 +1,25 @@
 package deakin.gopher.guardian.view.general
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.Query
-import com.google.android.material.navigation.NavigationView;
-import android.widget.ImageView;
-import androidx.drawerlayout.widget.DrawerLayout
 import deakin.gopher.guardian.R
 import deakin.gopher.guardian.adapter.TaskListAdapter
 import deakin.gopher.guardian.model.Task
 import deakin.gopher.guardian.services.NavigationService
-import androidx.core.view.GravityCompat
 
 
 class TasksListActivity : AppCompatActivity() {
@@ -33,6 +36,43 @@ class TasksListActivity : AppCompatActivity() {
         navigationView.setItemIconTintList(null);
         taskListMenuBtn.setOnClickListener({
             drawerLayout.openDrawer(GravityCompat.START);
+            navigationView.setNavigationItemSelectedListener { menuItem: MenuItem ->
+                val id = menuItem.itemId
+                if (R.id.nav_home === id) {
+                    startActivity(
+                        Intent(
+                            this@TasksListActivity,
+                            Homepage4caretaker::class.java
+                        )
+                    )
+                } else if (R.id.nav_admin === id) {
+                    startActivity(
+                        Intent(
+                            this@TasksListActivity,
+                            Homepage4caretaker::class.java
+                        )
+                    )
+                } else if (R.id.nav_settings === id) {
+                    startActivity(
+                        Intent(
+                            this@TasksListActivity,
+                            Setting::class.java
+                        )
+                    )
+                } else if (R.id.add_task === id) {
+                    startActivity(
+                        Intent(
+                            this@TasksListActivity,
+                            TasksListActivity::class.java
+                        )
+                    )
+                }else if (R.id.nav_signout === id) {
+                    FirebaseAuth.getInstance().signOut()
+                    startActivity(Intent(this@TasksListActivity, LoginActivity::class.java))
+                    finish()
+                }
+                true
+            }
         });
 
         val taskListRecyclerView: RecyclerView = findViewById(R.id.task_list_recycleView)
