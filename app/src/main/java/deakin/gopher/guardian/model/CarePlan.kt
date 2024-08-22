e203fc48f3920b10f933769c62d1e028a14b7c1c
mirahazall <s223530601@deakin.edu.au>
2024-08-22 10:34:04 +1000

    Fix code formatting issues in CarePlan file using Spotless

--- app/src/main/java/deakin/gopher/guardian/model/CarePlan.kt
+++ app/src/main/java/deakin/gopher/guardian/model/CarePlan.kt
@@ -4,21 +4,29 @@ class CarePlan  // Constructors, getters, and setters
 {
     @JvmField
     var behavioralManagement: String? = null
+
     @JvmField
     var carePlanType: String? = null
+
     @JvmField
     var dietTimings: String? = null
+
     @JvmField
     var drinkLikings: String? = null
+
     @JvmField
     var nutritionHydration: String? = null
+
     @JvmField
     var painCategories: String? = null
+
     @JvmField
     var painScore: Int = 0
     var patientId: String? = null
+
     @JvmField
     var sleepPattern: String? = null
+
     @JvmField
     var supportRequirement: String? = null
 }
