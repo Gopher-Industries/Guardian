1642fb7c55808a8e07ac7f08625105fff73db437
musab007 <musabvohra@aminsofttech.co>
2024-08-27 12:40:20 +0530

    login button remove and google login error solve

---
+++ app/src/main/java/deakin/gopher/guardian/view/general/BloodPressureActivity.kt
@@ -0,0 +1,12 @@
+package deakin.gopher.guardian.view.general
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import deakin.gopher.guardian.R
+
+class BloodPressureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blood_pressure)
+    }
+}
