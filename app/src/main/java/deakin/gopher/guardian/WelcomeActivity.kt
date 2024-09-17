b0f4508566eaa2727ad0c564476c227321285b26
wendy Apiyo <wendyapiyo@gmail.com>
2024-09-17 13:56:22 +1000

    Add training feature

---
+++ app/src/main/java/deakin/gopher/guardian/WelcomeActivity.kt
@@ -0,0 +1,11 @@
+package deakin.gopher.guardian
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)  // This links to your activity_welcome.xml
+    }
+}
