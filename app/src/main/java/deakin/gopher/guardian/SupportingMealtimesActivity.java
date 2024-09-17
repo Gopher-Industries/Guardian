fb52df26c083ff8408717905f907baa62ed9898d
wendy Apiyo <wendyapiyo@gmail.com>
2024-09-17 13:56:22 +1000

    Add training feature

---
+++ app/src/main/java/deakin/gopher/guardian/SupportingMealtimesActivity.java
@@ -0,0 +1,14 @@
+package deakin.gopher.guardian;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SupportingMealtimesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supporting_mealtimes);
+    }
+}
+
