7ead4abc5925ae35e7d3eb910e833e153a9ae42d
onwa <amehugochukwu@gmail.com>
2024-09-21 10:04:00 +1000

    Add API integration for user registration

---
+++ app/src/main/java/deakin/gopher/guardian/model/register/User.kt
@@ -0,0 +1,22 @@
+package deakin.gopher.guardian.model.register
+
+import com.google.gson.annotations.SerializedName
+
+data class User(
+    @SerializedName("id") val id: String,
+    @SerializedName("email") val email: String,
+    @SerializedName("fullname") val name: String,
+    @SerializedName("role") val roleName: String,
+)
+
+data class RegisterRequest(
+    @SerializedName("email") val email: String,
+    @SerializedName("password") val password: String,
+    @SerializedName("fullname") val name: String,
+    @SerializedName("role") val role: String,
+)
+
+data class RegisterResponse(
+    @SerializedName("user") val user: User,
+    @SerializedName("token") val token: String,
+)
---
+++ app/src/main/java/deakin/gopher/guardian/services/api/ApiService.kt
@@ -0,0 +1,12 @@
+package deakin.gopher.guardian.services.api
+
+import deakin.gopher.guardian.model.register.RegisterRequest
+import deakin.gopher.guardian.model.register.User
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("users/register")
+    fun register(@Body request: RegisterRequest): Call<User>
+}
