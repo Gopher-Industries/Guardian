7eceb629bad0b979c4c4ad02b98bae3cf55f058c
garyzhang75 <166787037+garyzhang75@users.noreply.github.com>
2024-12-23 16:33:47 +1100

    research WiFi CSI and phase data

--- app/src/main/java/deakin/gopher/guardian/view/general/MainActivity.kt
+++ app/src/main/java/deakin/gopher/guardian/view/general/MainActivity.kt
@@ -2,19 +2,51 @@ package deakin.gopher.guardian.view.general
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Task
+import com.google.firebase.messaging.FirebaseMessaging
 import deakin.gopher.guardian.R
+import deakin.gopher.guardian.model.login.SessionManager
+import deakin.gopher.guardian.services.EmailPasswordAuthService
+import deakin.gopher.guardian.view.FallDetection.FallAlertActivity
+import deakin.gopher.guardian.view.caretaker.notifications.confirmincident.ConfirmIncidentActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
 
-        val getStartedButton: Button = findViewById(R.id.getStartedButton)
-        getStartedButton.setOnClickListener {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+           getStartedButton.setOnClickListener { _ -> onGetStartedClick() }
+
+            FirebaseMessaging.getInstance()
+                 .token
+                 .addOnCompleteListener { task: Task<String?> ->
+                     if (task.isSuccessful) {
+                         val token = task.result
+                         Log.d("FCM Token", token ?: "Token is null")
+                     } else {
+                         Log.w("MainActivity", "Fetching FCM registration token failed", task.exception)
+                     }
+                 }
+
+
+        getStartedButton.setOnClickListener{
+            startActivity(Intent(this@MainActivity, Homepage4caretaker::class.java))
         }
+    }
+
+    private fun onGetStartedClick() {
+        if (!SessionManager.isLoggedIn) {
+            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        } else {
+            startActivity(Intent(this@MainActivity, Homepage4caretaker::class.java))
+        }
+    }
+
+    private fun onLogoutClick() {
+        EmailPasswordAuthService.signOut(this)
+        finish()
     }
 }
