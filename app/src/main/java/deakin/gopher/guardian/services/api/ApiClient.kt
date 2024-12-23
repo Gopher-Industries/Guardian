b1fd17506ee8bc42b1393c2834646fd495661451
garyzhang75 <166787037+garyzhang75@users.noreply.github.com>
2024-12-23 14:26:40 +1100

    Enhanced research details on WiFi CSI and phase data.

--- app/src/main/java/deakin/gopher/guardian/services/api/ApiClient.kt
+++ app/src/main/java/deakin/gopher/guardian/services/api/ApiClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 //    private const val BASE_URL = "http://10.0.2.2:3000/api/v1/"
-    private const val BASE_URL = "https://guardian-backend-kz54.onrender.com/api/v1/"
+    private const val BASE_URL = "http://10.0.2.2:3000/api/v1/"
 
     private val client = OkHttpClient()
     private val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
--- app/src/main/java/deakin/gopher/guardian/view/general/MainActivity.kt
+++ app/src/main/java/deakin/gopher/guardian/view/general/MainActivity.kt
@@ -2,44 +2,19 @@ package deakin.gopher.guardian.view.general
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import com.google.android.gms.tasks.Task
-import com.google.firebase.messaging.FirebaseMessaging
+import androidx.appcompat.app.AppCompatActivity
 import deakin.gopher.guardian.R
-import deakin.gopher.guardian.model.login.SessionManager
-import deakin.gopher.guardian.services.EmailPasswordAuthService
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
 
-        getStartedButton.setOnClickListener { _ -> onGetStartedClick() }
-
-        FirebaseMessaging.getInstance()
-            .token
-            .addOnCompleteListener { task: Task<String?> ->
-                if (task.isSuccessful) {
-                    val token = task.result
-                    Log.d("FCM Token", token ?: "Token is null")
-                } else {
-                    Log.w("MainActivity", "Fetching FCM registration token failed", task.exception)
-                }
-            }
-    }
-
-    private fun onGetStartedClick() {
-        if (!SessionManager.isLoggedIn) {
-            startActivity(Intent(this@MainActivity, LoginActivity::class.java))
-        } else {
-            startActivity(Intent(this@MainActivity, Homepage4caretaker::class.java))
+        val getStartedButton: Button = findViewById(R.id.getStartedButton)
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
-    }
-
-    private fun onLogoutClick() {
-        EmailPasswordAuthService.signOut(this)
-        finish()
     }
 }
