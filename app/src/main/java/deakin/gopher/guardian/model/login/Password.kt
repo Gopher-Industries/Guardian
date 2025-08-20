2332c6bd0c11d5673c8f13563b353b2cd7330757
Biswas Biswajeet Dash <biswajeetdash01@gmail.com>
2025-08-20 21:27:28 +1000

    Update Password.kt
    updated

--- app/src/main/java/deakin/gopher/guardian/model/login/Password.kt
+++ app/src/main/java/deakin/gopher/guardian/model/login/Password.kt
@@ -1,24 +1,20 @@
 package deakin.gopher.guardian.model.login
 
 data class Password(val password: String) {
-    fun isValid(): Boolean {
-        if (password.isEmpty()) {
-            return false // Password is empty, not valid
-        }
 
-        val minLength = 12
+    fun isValid(): Boolean {
+        val minLength = 8
+        val maxLength = 16
+
+        if (password.length !in minLength..maxLength) return false
+
         val hasUppercase = password.any { it.isUpperCase() }
         val hasLowercase = password.any { it.isLowerCase() }
         val hasDigit = password.any { it.isDigit() }
-
         val allowedSpecialChars = setOf('!', '@', '#', '$', '%', '^', '&', '*')
         val hasSpecialChar = password.any { it in allowedSpecialChars }
 
-        return password.length >= minLength &&
-            hasUppercase &&
-            hasLowercase &&
-            hasDigit &&
-            hasSpecialChar
+        return hasUppercase && hasLowercase && hasDigit && hasSpecialChar
     }
 
     fun confirmWith(other: String): Boolean {
