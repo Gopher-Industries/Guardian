68c88159b522ec071c6d4b9dfad3edba38f4c29a
vincent serem <s223694903@deakin.edu.au>
2026-05-23 15:53:39 +1000

    fixed unimported component

--- guardian-admin-dashboard/src/App.jsx
+++ guardian-admin-dashboard/src/App.jsx
@@ -14,6 +14,7 @@ import SettingsPage from "./pages/SettingsPage";
 import DoctorAssignmentsPage from "./pages/DoctorAssignmentsPage";
 import "./App.css";
 import PatientOverviewPage from "./pages/PatientOverviewPage";
+import SupportTicketPage from "./pages/SupportTicketPage";
 
 function ProtectedRoute({ children }) {
   const token = getAuthToken();
