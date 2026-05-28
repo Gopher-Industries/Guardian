8fa5f8359896c854b9ce2d59f5738e8223d0f3fa
Kudrat Arora <kudratarora@10-141-8-51.wifi-m.deakin.edu.au>
2026-05-28 11:34:56 +1000

    Apply spotless formatting for CI

--- app/src/main/java/deakin/gopher/guardian/view/patient/dailyreport/DailyReportActivity.java
+++ app/src/main/java/deakin/gopher/guardian/view/patient/dailyreport/DailyReportActivity.java
@@ -15,61 +15,60 @@ import deakin.gopher.guardian.view.general.LoginActivity;
 
 public class DailyReportActivity extends AppCompatActivity {
 
-    ImageView dailyReportMenuButton;
+  ImageView dailyReportMenuButton;
 
-    @Override
-    protected void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_daily_report);
+  @Override
+  protected void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_daily_report);
 
-        final NavigationView navigationView = findViewById(R.id.nav_view);
-        dailyReportMenuButton = findViewById(R.id.menuButton11);
-        final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView.setItemIconTintList(null);
+    final NavigationView navigationView = findViewById(R.id.nav_view);
+    dailyReportMenuButton = findViewById(R.id.menuButton11);
+    final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+    navigationView.setItemIconTintList(null);
 
-        navigationView.setNavigationItemSelectedListener(
-                menuItem -> {
-                    Intent intent = null;
-                    final int itemId = menuItem.getItemId();
+    navigationView.setNavigationItemSelectedListener(
+        menuItem -> {
+          Intent intent = null;
+          final int itemId = menuItem.getItemId();
 
-                    if (itemId == R.id.nav_home) {
-                        final boolean isAdmin =
-                                deakin.gopher.guardian.model.login.SessionManager.INSTANCE
-                                                .getCurrentUser()
-                                                .getRole()
-                                        instanceof deakin.gopher.guardian.model.login.Role.Admin;
-                        intent =
-                                new Intent(
-                                        DailyReportActivity.this,
-                                        isAdmin ? Homepage4admin.class : Homepage4caretaker.class);
-                    } else if (itemId == R.id.nav_signout) {
-                        com.google.firebase.auth.FirebaseAuth.getInstance().signOut();
-                        intent = new Intent(DailyReportActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                        finish();
-                        return true;
-                    }
+          if (itemId == R.id.nav_home) {
+            final boolean isAdmin =
+                deakin.gopher.guardian.model.login.SessionManager.INSTANCE
+                        .getCurrentUser()
+                        .getRole()
+                    instanceof deakin.gopher.guardian.model.login.Role.Admin;
+            intent =
+                new Intent(
+                    DailyReportActivity.this,
+                    isAdmin ? Homepage4admin.class : Homepage4caretaker.class);
+          } else if (itemId == R.id.nav_signout) {
+            com.google.firebase.auth.FirebaseAuth.getInstance().signOut();
+            intent = new Intent(DailyReportActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+            return true;
+          }
 
-                    if (intent != null) {
-                        startActivity(intent);
-                    }
+          if (intent != null) {
+            startActivity(intent);
+          }
 
-                    if (drawerLayout != null) {
-                        drawerLayout.closeDrawer(GravityCompat.START);
-                    }
-                    return true;
-                });
+          if (drawerLayout != null) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+          }
+          return true;
+        });
 
-        dailyReportMenuButton.setOnClickListener(
-                v -> drawerLayout.openDrawer(GravityCompat.START));
+    dailyReportMenuButton.setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
 
-        final String patientNameExtra = getIntent().getStringExtra("patientName");
-        final String patientName = patientNameExtra != null ? patientNameExtra.split(" ")[0] : "";
+    final String patientNameExtra = getIntent().getStringExtra("patientName");
+    final String patientName = patientNameExtra != null ? patientNameExtra.split(" ")[0] : "";
 
-        final TextView usernameTextView = findViewById(R.id.username);
+    final TextView usernameTextView = findViewById(R.id.username);
 
-        /*if (null != patientName) {
-          usernameTextView.setText(patientName);
-        }*/
-    }
+    /*if (null != patientName) {
+      usernameTextView.setText(patientName);
+    }*/
+  }
 }
